6adddc33a3a9b19eb28e7052fcf4e01e1f51a286
houlile <2063473026@qq.com>
2022-11-03 12:11:50 +0800

    OLED显示温湿度采集数据

---
+++ User/main.c.c
@@ -0,0 +1,45 @@
+#include "System.h"
+#include "Delay.h"
+#include "UART.h"
+#include "AHT20.h"
+#include "gui.h"
+
+struct AHT20_INF AHT20;
+
+u32 Temp = 0;
+u32 Hum = 0;
+
+int main(void)
+{
+	//-------------------
+	//oled相关初始化
+		delay_init();	    	      
+		NVIC_Configuration(); 	   
+		OLED_Init();			         
+		OLED_Clear(0);
+		
+		
+  //-------------------	
+		IIC_Init();
+    Delay_Init();
+		My_Uart_Init(115200);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOC,ENABLE);
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_Init(GPIOC,&GPIO_InitStructure);
+		GPIO_ResetBits(GPIOC,GPIO_Pin_13);
+
+		AHT20.alive = !AHT20_Init();
+    while(1)
+    {
+		if(AHT20.alive)
+		{
+			AHT20.flag = AHT20_ReadHT(AHT20.ht);
+			StandardUnitCon(&AHT20);
+		}
+		
+    }
+}
